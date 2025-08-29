b7dcac8e10bc839b9f586ac0f4de6c90df0a5ece
Soumadip <39thdecember@gmail.com>
2025-08-29 22:01:04 +0530

    Owner/Admin Dashboard ✅✅✅

--- client/src/pages/dashboard/Dashboard.jsx
+++ client/src/pages/dashboard/Dashboard.jsx
@@ -100,7 +100,14 @@ function Dashboard() {
           ))}
         </div>
         {/* monthly revenue */}
-        <div className=""></div>
+        <div className="p-4 md:p-6 border border-borderColor rounded-md w-full md:max-w-xs">
+          <h1 className="text-lg font-medium">Monthly Revenue</h1>
+          <p className="text-gray-500">Revenue for current month</p>
+          <p className="text-3xl mt-6 font-semibold text-primary">
+            {currency}
+            {data.monthlyRevenue}
+          </p>
+        </div>
       </div>
     </div>
   );
